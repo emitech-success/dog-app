68d61cf158996feee1bf6bae526f5005d78875fb
Odezulu Chukwuemeka <odezuluaugustine@gmail.com>
2024-01-05 08:52:12 +0100

    changed somestyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+
 import { QueryClient, QueryClientProvider } from 'react-query';
 import DogBreedSelector from './component/DogBreedSelector';
 
@@ -8,7 +8,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center">
         <DogBreedSelector />
       </div>
     </QueryClientProvider>
